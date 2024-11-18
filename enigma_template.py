c57af365f0179e09ae32ddaee50946075a74a11b
Youssief Abdalla <abda8926@student.acitech.org>
2024-11-18 10:48:04 -0500

    11.18.24
    added some of the first function
    nothing works
    must finish everything :-1:

--- enigma_template.py
+++ enigma_template.py
@@ -1,30 +1,47 @@
 # enigma.py
 # description: a simple rotational ciphertext program that can create
 # custom encoded messages, as well as encode and decode from file.
-# author: YOUR_NAME_HERE
-# created: MM.DD.YYYY
-# last update:  MM.DD.YYYY
+# author: Youssief Abdalla
+# created: 11.18.2024
+# last update:  11.18.2024
 import random
 
 # we'll be using this string for the majority of our translations
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+
 # user inputs a message and selects a key (or random), the message is then translated using the cipher
 def encode_message():
-    pass
+    msg = str(input("Input Message here: ")).lower()
+    key = input("Input the rotational cipher key: (Press enter for random value)")
+    newmsg = ""
+    if key == "":
+        key = random.randint(0, 25)
+    try:
+        key = int(key)
+    except ValueError:
+        print("You did not enter a valid value, key being randomized...")
+        key = random.randint(0, 25)
+    for i in range(len(msg)):
+        letter = alphabet[(i % 26 + key) % 26]
+        newmsg += letter
+    print(f"Used key of {key} your new message is : {newmsg}")
+
 
 # encodes a target file, similarly to encode_message, except now targeting a filename
 def encode_file():
     pass
+
 
 # decodes target file using a user-specified key. If key is unknown, a keypress should
 # call decode_unknown_key()
 def decode_file():
     pass
 
+
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
 def decode_unknown_key(filename):
-   pass
+    pass
 
 
 # main method declaration
@@ -50,6 +67,7 @@ def main():
             exit()
         else:
             print("Invalid choice. Please try again.")
+
 
 # runs on program start
 if __name__ == "__main__":
